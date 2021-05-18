b77db091a13f386e796936c12670850a606af723
artemgur <artemgur01@gmail.com>
2021-05-18 16:23:42 +0300

    Update Tests.cs

--- Null_EqualsHashCode/Tests.cs
+++ Null_EqualsHashCode/Tests.cs
@@ -9,10 +9,13 @@ namespace Null_EqualsHashCode
         public void DateTest()
         {
             var date = new Date(18, 5);
+            
             var set = new HashSet<Date>();
             set.Add(date);
+            
             var sameDate = new Date(18, 5);
             Assert.Equal(date, sameDate);
+            
             Assert.True(set.Contains(date));
             Assert.False(set.Contains(sameDate));
         }
